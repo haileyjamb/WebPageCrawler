43f691985c3514e92bb2d190ebbea57c1b19fbde
Hyerin Jo <21500701@handong.edu>
2018-06-21 16:01:45 +0900

    gradle solved!

--- src/main/java/edu/handong/csee/java/webcrawler/HTMLCreator.java
+++ src/main/java/edu/handong/csee/java/webcrawler/HTMLCreator.java
@@ -1,5 +1,28 @@
 package edu.handong.csee.java.webcrawler;
 
-public class HTMLCreator {
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class HTMLCreator {
+	
+	public void saveAsHTML(String dir, String siteURL) {
+		
+		try {
+			
+			BufferedWriter bw = new BufferedWriter(new FileWriter(new File(dir + "/index.html")));
+			
+			bw.write(siteURL);
+			
+			System.out.println("File created @ " + dir);
+			bw.close();
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+			
+		}
+
+	}
 }
--- src/main/java/edu/handong/csee/java/webcrawler/URLReader.java
+++ src/main/java/edu/handong/csee/java/webcrawler/URLReader.java
@@ -1,5 +1,38 @@
 package edu.handong.csee.java.webcrawler;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.HttpURLConnection;
+
 public class URLReader {
 
-}
+	public String merger(String url) {
+
+		BufferedReader in = null;
+		String mergedLines = null;
+
+		try {
+
+			URL page = new URL(url); 
+			HttpURLConnection connection = (HttpURLConnection) page.openConnection();
+
+			connection.setRequestMethod("GET");
+
+			in = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
+
+			String lines;
+			while ((lines = in.readLine()) != null) {
+				mergedLines += (lines + "\n");
+
+			}
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+
+		}
+		
+		return mergedLines;
+	}
+}//https://docs.oracle.com/javase/tutorial/networking/urls/connecting.html
